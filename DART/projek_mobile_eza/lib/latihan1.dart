f531a40416152a9300576928e6aa25b336a1fb15
eza2910 <163159765+eza2910@users.noreply.github.com>
2025-08-26 11:42:37 +0900

    aplikasi biodata

---
+++ DART/projek_mobile_eza/lib/latihan1.dart
@@ -0,0 +1,17 @@
+  void main(){
+    var name = "M. Alfarezha R.Hi.Husain";
+     int kelahiran=2005;
+     double umur=19.10;
+     var tempattinggal = "jambula";
+      num tinggi = 170.5;
+      dynamic berat =75;
+      bool isActive=true;
+      print("nama :$name");///pake tanda {} jika ingin memunculkan objek lebih dari satu
+      print("tahun kelahiran :$kelahiran");
+      print("umur :$umur");
+      print("tempat tinggal :$tempattinggal");
+      print("tinggi :$tinggi");
+      print("berat :$berat");
+      print("status mahasiswa:$isActive");
+      print("apakah berat = tinggi : ${umur==tinggi}");
+  }
